e063d27b2618b022bd02435c77c02079dd87305d
Bobovskiy Konstantin <kbobovskiy@yandex.ru>
2025-01-16 17:36:12 +0100

    Add linters and prettier

--- webpack.config.js
+++ webpack.config.js
@@ -3,13 +3,13 @@ const path = require('path');
 const buildPath = path.resolve(__dirname, 'dist');
 const sourcePath = path.resolve(__dirname, 'src');
 
-const CopyPlugin = require("copy-webpack-plugin");
+const CopyPlugin = require('copy-webpack-plugin');
 
 module.exports = {
     mode: 'production',
     entry: {
         background: `${sourcePath}/background/background.js`,
-        content: `${sourcePath}/content/content.js`
+        content: `${sourcePath}/content/content.js`,
     },
     output: {
         path: buildPath,
@@ -30,21 +30,21 @@ module.exports = {
             },
             {
                 test: /\.css$/,
-                use: ['style-loader', 'css-loader']
-            }
+                use: ['style-loader', 'css-loader'],
+            },
         ],
     },
     resolve: {
-        extensions: ['.js']
+        extensions: ['.js'],
     },
     plugins: [
         new CopyPlugin({
-        patterns: [
-            { from: `${sourcePath}/manifest.json`, to: `${buildPath}/` },
-            { from: `${sourcePath}/views`, to: `${buildPath}/views` },
-            { from: `${sourcePath}/icons`, to: `${buildPath}/icons` },
-            { from: `${sourcePath}/scripts`, to: `${buildPath}/scripts` },
-            { from: `${sourcePath}/styles`, to: `${buildPath}/styles` },
+            patterns: [
+                { from: `${sourcePath}/manifest.json`, to: `${buildPath}/` },
+                { from: `${sourcePath}/views`, to: `${buildPath}/views` },
+                { from: `${sourcePath}/icons`, to: `${buildPath}/icons` },
+                { from: `${sourcePath}/scripts`, to: `${buildPath}/scripts` },
+                { from: `${sourcePath}/styles`, to: `${buildPath}/styles` },
             ],
         }),
     ],
